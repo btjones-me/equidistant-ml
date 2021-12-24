a055360824f979fb5c94d5754da055b27d8499e8
Benjamin Jones <btjones.me@gmail.com>
2021-12-24 02:20:45 +0000

    update app to new schema

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ poetry install
 * Create a linear approximator as a baseline model
 * Some inspiration can be taken from:
 https://towardsdatascience.com/simple-example-of-2d-density-plots-in-python-83b83b934f67
-
+* Add tests in GitHub actions CI
 ## Credits
 
 This package was created with Cookiecutter and the `btjones-me/cookiecutter-pypackage` project template.
--- equidistant_ml/noise/generator.py
+++ equidistant_ml/noise/generator.py
@@ -6,14 +6,15 @@ import matplotlib.pyplot as plt
 
 class GenerateGaussian:
 
-    def __init__(self, x_bounds=(-10, 10), y_bounds=(-10, 10), sigma_x=5., sigma_y=5., size=1000):
-        self.size = size
-        self.x, self.y, self.z = self.get_gaussian(x_bounds, y_bounds, sigma_x, sigma_y, size)
+    def __init__(self, x_bounds=(-10, 10), y_bounds=(-10, 10), sigma_x=5., sigma_y=5., x_size=1000, y_size=1000):
+        self.x_size = x_size
+        self.y_size = y_size
+        self.x, self.y, self.z = self.get_gaussian(x_bounds, y_bounds, sigma_x, sigma_y, x_size, y_size)
 
     @staticmethod
-    def get_gaussian(x_bounds=(-10,10), y_bounds=(-10,10), sigma_x=5., sigma_y=5., size=1000):
-        x = np.linspace(x_bounds[0], x_bounds[1], size)
-        y = np.linspace(y_bounds[0], y_bounds[1], size)
+    def get_gaussian(x_bounds=(-10, 10), y_bounds=(-10, 10), sigma_x=5., sigma_y=5., x_size=1000, y_size=1000):
+        x = np.linspace(x_bounds[0], x_bounds[1], x_size)
+        y = np.linspace(y_bounds[0], y_bounds[1], y_size)
         x, y = np.meshgrid(x, y)
         z = (1/(2*np.pi*sigma_x*sigma_y) * np.exp(-(x**2/(2*sigma_x**2)
              + y**2/(2*sigma_y**2))))
@@ -50,13 +51,19 @@ class GenerateGaussian:
 
 class CoordinateDummy(GenerateGaussian):
 
-    def __init__(self, size=1000):
-        super().__init__(size=size)
+    def __init__(self, x0=None, x1=None, y0=None, y1=None, x_size=1000, y_size=1000):
+        super().__init__(x_size=x_size, y_size=y_size)
 
-        from equidistant_ml.config import cricklewood_coords, kidbrooke_coords
+        if any(val is None for val in (x0, x1, y0, y1)):
+            # use defaults
+            from equidistant_ml.config import cricklewood_coords, kidbrooke_coords
+            x0 = cricklewood_coords['lng']
+            x1 = kidbrooke_coords['lng']
+            y0 = cricklewood_coords['lat']
+            y1 = kidbrooke_coords['lat']
 
-        xs = np.linspace(cricklewood_coords['lng'], kidbrooke_coords['lng'], self.size)
-        ys = np.linspace(cricklewood_coords['lat'], kidbrooke_coords['lat'], self.size)
+        xs = np.linspace(x0, x1, self.x_size)
+        ys = np.linspace(y0, y1, self.y_size)
 
         self.df = pd.DataFrame(self.z)
         self.df.columns = np.round(xs, 5)
